f713275c3f800ac0e887c4e60ce8df27a0d467d7
DedeNanda <kirihime0852@gmail.com>
2024-12-12 20:52:15 +0700

    update view history value

--- resources/views/users/history_value_users.blade.php
+++ resources/views/users/history_value_users.blade.php
@@ -5,9 +5,33 @@
 {{-- history nilai user --}}
 <div class="history-value-user">
   <h2>Quiz App</h2>
-  <h2>History nilai (nama user)</h2>
+  <h2>History nilai {{ Auth::user()->name }}</h2>
+  
   <table>
-    sini tabel
+    <thead>
+      <tr>
+        <th>Mata Pelajaran</th>
+        <th>Nilai</th>
+        <th>Status</th>
+      </tr>
+    </thead>
+    <tbody>
+      @foreach ($history_ips as $ips)
+    <tr>
+      <td>IPS</td>
+      <td>{{ $history_ips->value_result ?? 'Data tidak tersedia' }}</td>
+      <td>{{ $history_ips && $history_ips->value_result >= 75 ? 'Selamat! Anda lulus dengan nilai yang sangat baik.' : 'Mohon maaf, Anda belum lulus. Silakan coba lagi.' }}</td>
+    </tr>
+  @endforeach
+
+  @foreach ($history_ipa as $ipa)
+    <tr>
+      <td>IPA</td>
+      <td>{{ $history_ipa->value_result ?? 'Data tidak tersedia' }}</td>
+    <td>{{ $history_ipa && $history_ipa->value_result >= 75 ? 'Selamat! Anda lulus dengan nilai yang sangat baik.' : 'Mohon maaf, Anda belum lulus. Silakan coba lagi.' }}</td>
+    </tr>
+  @endforeach
+    </tbody>
   </table>
   <a href="{{ route('halaman_user') }}"><button>Home</button></a>
 </div>
