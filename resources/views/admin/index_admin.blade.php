0e536e5b471079c67fd2aeea60fafd53f44a4d27
DedeNanda <kirihime0852@gmail.com>
2024-12-14 21:01:48 +0700

    update UI admin for Quiz

--- resources/views/admin/index_admin.blade.php
+++ resources/views/admin/index_admin.blade.php
@@ -1,42 +1,9 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="shortcut icon" href="{{ asset("image/icon.png") }}" type="image/x-icon">
-    <title>ini admin</title>
+@extends('admin.admin_layout.admin_header')
+{{-- untuk menampilkan header --}}
 
-    <link rel="stylesheet" href="{{ asset('css/admin.css') }}">
-</head>
-<body>
-    <div class="sidebar">
-        <h2>Admin Panel</h2>
-        <ul>
-            <li><a href="#">Dashboard</a></li>
-            <li><a href="#">Users</a></li>
-            <li><a href="#">Analytics</a></li>
-            <li><a href="#">Messages</a></li>
-            <li><a href="#">Settings</a></li>
-            <li><a href="#">Logout</a></li>
-        </ul>
-    </div>
-    <div class="main">
-        <div class="header d-flex align-items-center">
-            <h1>Welcome, Admin</h1>
-            <div class="search-bar">
-                <input type="text" placeholder="Search...">
-            </div>
-            <div class="profile-dropdown ms-3">
-                <img src="https://via.placeholder.com/50" alt="Admin" data-bs-toggle="dropdown" aria-expanded="false">
-                <ul class="dropdown-menu">
-                    <li><a class="dropdown-item" href="#">Change Profile</a></li>
-                    <li><a class="dropdown-item" href="#">Change Password</a></li>
-                    <li><hr class="dropdown-divider"></li>
-                    <li><a class="dropdown-item" href="#">Log Out</a></li>
-                </ul>
-            </div>
-        </div>
+@section('content')
+{{-- isi conten lanjutan dari header --}}
+
         <div class="cards">
             <div class="card">
                 <h3>Total Users</h3>
@@ -55,8 +22,9 @@
                 <p>$7,890</p>
             </div>
         </div>
-    </div>
 
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>
-</body>
-</html>
+{{-- bawah ini menampilkan footer --}}
+@include('admin.admin_layout.admin_footer')
+
+{{-- end conten --}}
+@endsection
