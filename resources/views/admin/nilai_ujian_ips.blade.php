4179ae17eedf89c4b7d363ca17fb6878064c9e62
DedeNanda <kirihime0852@gmail.com>
2024-12-15 20:07:08 +0700

    init add view in addmin

---
+++ resources/views/admin/nilai_ujian_ips.blade.php
@@ -0,0 +1,13 @@
+@extends('admin.admin_layout.admin_header')
+{{-- untuk menampilkan header --}}
+
+@section('content')
+{{-- isi conten lanjutan dari header --}}
+
+       <h1>halaman nilai ujian ips</h1>
+
+{{-- bawah ini menampilkan footer --}}
+@include('admin.admin_layout.admin_footer')
+
+{{-- end conten --}}
+@endsection
