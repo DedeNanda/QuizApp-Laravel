a1a85f7fd01dea89c987c2ea9389f4d39e19e0aa
DedeNanda <kirihime0852@gmail.com>
2024-12-16 20:00:09 +0700

    update controller admin

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -28,4 +28,48 @@ class AdminController extends Controller
 
         return view('admin.change_password_admin', $data);
     }
+
+    //tampilan change profile admin
+    public function change_profile_admin()
+    {
+
+        $data = array(
+            'title' => 'Change Profile Admin',
+        );
+
+        return view('admin.change_profile_admin', $data);
+    }
+
+    //tampilan nilai ujian ipa 
+    public function nilai_ujian_ipa()
+    {
+
+        $data = array(
+            'title' => 'Nilai Ujian Ipa',
+        );
+
+        return view('admin.nilai_ujian_ipa', $data);
+    }
+
+    //tampilan nilai ujian ips
+    public function nilai_ujian_ips()
+    {
+
+        $data = array(
+            'title' => 'Nilai Ujian Ips',
+        );
+
+        return view('admin.nilai_ujian_ips', $data);
+    }
+
+    //tampilan melihat user 
+    public function melihat_user()
+    {
+
+        $data = array(
+            'title' => 'Melihat User',
+        );
+
+        return view('admin.melihat_user', $data);
+    }
 }
